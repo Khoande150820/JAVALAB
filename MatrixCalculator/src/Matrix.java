189fea2fff8bbf39c2c41b241bfe0a0f2bd91940
Khoande150820 <72441554+Khoande150820@users.noreply.github.com>
2020-10-28 21:51:53 +0700

    Update Matrix.java

--- MatrixCalculator/src/Matrix.java
+++ MatrixCalculator/src/Matrix.java
@@ -76,26 +76,36 @@ public class Matrix {
         int[][] matrix1 = getMatrix(1);
         int[][] matrix2 = getMatrix(2);
         int row1 = matrix1.length;
+        int row2 = matrix1[0].length;
+        int col1 = matrix2.length;
         int col2 = matrix2[0].length;
-        if (row1 != col2){
+
+        if (row2 != col1) {
             System.out.println("Can't multiply");
             return;
         }
-        int [][] result = new int[row1][col2];
-        
-        for (int i = 0; i < row1; i++){
-            for (int j = 0; j < col2;j++){
-                for(int k = 0; k < row1;k++){
-                    result[i][k] += matrix1[i][k]*matrix2[k][j];
+        int[][] result = new int[row1][col2];
+        // Iterate over rows of the resulting matrix
+        for (int i = 0; i < row1; i++) {
+            // Iterate over each entry in the resulting matrix
+            for (int j = 0; j < col2; j++) {
+                System.out.print("[");
+                // Assign each entry the dot product between row i of first matrix and column j of second matrix
+                for (int k = 0; k < row2; k++) {
+                    System.out.print(matrix1[i][k] + "*" + matrix2[k][j]);
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                    System.out.print("+");
                 }
+                System.out.print("]");
             }
+            System.out.println("");
         }
-        
+
         printMatrix(matrix1);
         System.out.println("x");
         printMatrix(matrix2);
         System.out.println("=");
         printMatrix(result);
-        
+
     }
 }
